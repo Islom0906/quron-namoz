c401d40cfaf00f608a244bc1c36027d880a69037
islom0906 <iabdugofurov99@gmail.com>
2022-12-26 22:54:07 +0500

    add hover

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div className="bg-bg w-full min-h-screen">
       <Navbar/>
       <Sidebar/>
-      <main className='pt-[104px] sm:pl-[104px] pl-10 pb-10 pr-10'>
+      <main className='pt-[104px] sm:pl-[104px] pl-10 sm:pb-10 pb-20 pr-10'>
       <Routes>
       <Route path='/' element={<Surah/>} />
       </Routes>
