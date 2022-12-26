b24eb1ac87d4a729de5b734116b6e3ac93e0e070
islom0906 <iabdugofurov99@gmail.com>
2022-12-26 22:44:03 +0500

    add hover

--- src/components/surah-card/Surah-card.jsx
+++ src/components/surah-card/Surah-card.jsx
@@ -1,13 +1,18 @@
 const SurahCard = ({surah}) => {
   return ( 
-    <div className="p-5 bg-white rounded-xl w-full flex items-start flex-col space-y-7 cursor-pointer hover:shadow-2xl duration-300">
+    <div className="p-5 bg-white rounded-xl w-full flex items-start flex-col space-y-7 cursor-pointer group hover:bg-primary hover:shadow-2xl duration-300">
+      <div className="flex items-center justify-between w-full">
       <span className="px-3 py-1 text-spanColor rounded-full bg-spanBg">
         {surah.number}
       </span>
+      <span className="px-3 py-1 text-spanColor rounded-full bg-spanBg">
+      Ayahs: {surah.numberOfAyahs}
+      </span>
+      </div>
       <div className="context flex flex-col items-start space-y-2">
-        <p className="font-bold text-xl">{surah.name}</p>
-        <p className="font-bold text-xl opacity-50">{surah.englishName}</p>
-        <p className="font-bold text-xl opacity-50">
+        <p className="font-bold text-xl group-hover:text-white">{surah.name}</p>
+        <p className="font-bold text-xl group-hover:text-white opacity-50">{surah.englishName}</p>
+        <p className="font-bold text-xl group-hover:text-white opacity-50">
           {surah.englishNameTranslation}
         </p>
       </div>
