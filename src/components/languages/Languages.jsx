775130595e0c9e760f2fb4c76ae134d650ea9b66
islom0906 <iabdugofurov99@gmail.com>
2022-12-28 23:48:50 +0500

    add qorilar

--- src/components/languages/Languages.jsx
+++ src/components/languages/Languages.jsx
@@ -7,7 +7,7 @@ const Languages = () => {
   return (
     <div>
     
-      <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-20 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none" 
+      <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-20 p-2.5  outline-none" 
       onChange={(e) => dispatch(selectedLanguage(e.target.value))}>
         <option selected value="uz.sodik">Uz</option>
         <option value="ru.kuliev">Ru</option>
