2ab06efc62bb3fc3d5488eef003f827069f36d5b
islom0906 <iabdugofurov99@gmail.com>
2022-12-28 23:30:51 +0500

    add qorilar

--- src/components/author-audio/Author-audio.jsx
+++ src/components/author-audio/Author-audio.jsx
@@ -1,8 +1,8 @@
 const AuthorAudio = ({author,authorAudio}) => {
   return (
-    <div className="flex flex-col items-center space-y-3 cursor-pointer w-48 flex-shrink-0" onClick={()=> authorAudio(author.value)}>
+    <div className="flex flex-col items-center justify-between space-y-3 cursor-pointer sm:w-48 w-28 h-44 flex-shrink-0" onClick={()=> authorAudio(author.value)}>
       <img src={author.img} className="w-24 h-24 rounded-full"  alt="" />
-      <p className="text-base font-bold">{author.name}</p>
+      <p className="text-base font-bold text-center">{author.name}</p>
     </div>
   )
 }
