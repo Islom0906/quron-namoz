64eeaddb664be15f3900e5e8ead7c7d6a833cdf0
islom0906 <iabdugofurov99@gmail.com>
2022-12-30 14:38:45 +0500

    edit audioplayer

--- src/components/author-audio/Author-audio.jsx
+++ src/components/author-audio/Author-audio.jsx
@@ -1,6 +1,8 @@
+import './author-audio.css'
+
 const AuthorAudio = ({author,authorAudio}) => {
   return (
-    <div className="flex flex-col items-center justify-between space-y-3 cursor-pointer sm:w-48 w-28 h-44 flex-shrink-0" onClick={()=> authorAudio(author.value)}>
+    <div className="author-audio flex flex-col items-center justify-between space-y-3 cursor-pointer sm:w-48 w-28 h-44 flex-shrink-0" onClick={()=> authorAudio(author.value)}>
       <img src={author.img} className="w-24 h-24 rounded-full"  alt="" />
       <p className="text-base font-bold text-center">{author.name}</p>
     </div>
