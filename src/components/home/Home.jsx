d0b8c7e83918053510121838a671abf5299f4ee4
Islom <iabdugofurov99@gmail.com>
2023-01-20 23:46:50 +0500

    edit ui

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className='w-full h-screen overflow-y-hidden  flex items-center justify-center'>
-      <img className='w-full h-full' src="./assets/ilustrator.svg" alt="" />
+    <div className='w-full h-screen flex-col  flex items-center justify-center sm:space-y-0 space-y-10'>
+      <p className='text-primary sm:text-5xl text-3xl text-center font-bold '>Welcome to Islam</p>
+      <img className='w-full sm:h-full' src="./assets/ilustrator.svg" alt="" />
     </div>
   )
 }
